5f85dae4bd8e4831b55e568af3815a29f68dcd37
canthonyscott <anthony@canthonyscott.com>
2016-06-13 14:05:55 -0500

    prepared views and ViewHistory

---
+++ app/src/main/java/com/canthonyscott/microinjectioncalc/ViewHistory.java
@@ -0,0 +1,23 @@
+package com.canthonyscott.microinjectioncalc;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ViewHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_history);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+}
