7f4699f4477f54890a4774311d45f4d9ca7eaf8a
canthonyscott <anthony@canthonyscott.com>
2016-06-13 14:36:01 -0500

    History Adapter created

--- app/src/main/java/com/canthonyscott/microinjectioncalc/ViewHistory.java
+++ app/src/main/java/com/canthonyscott/microinjectioncalc/ViewHistory.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ViewHistory extends AppCompatActivity {
+
+    private  HistoryAdapter adapter;
+    private ArrayList<HistoryItem> historyArray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,9 @@ public class ViewHistory extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final ListView listViewMO = (ListView) findViewById(R.id.historyList);
+        adapter = new HistoryAdapter(this, R.layout.listview_history_item, historyArray);
 
 
     }
